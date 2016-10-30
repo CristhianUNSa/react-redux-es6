3067f434726ab7d147f9f5cbf3b546380d928e15
Cristhian Valencia <valenciacristhian47@gmail.com>
2016-10-30 07:32:51 -0300

    Routes - Rutas

---
+++ client/components/signup/SignupForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class SignupForm extends React.Component{
+  render(){
+    return(
+      <div>
+        SignupForm
+      </div>
+    )
+  }
+}
+export default SignupForm;
